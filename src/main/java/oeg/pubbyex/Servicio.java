03910035da08b928920c8b04f025582e4417a986
Victor Rodriguez Doncel <victorr@ac.upc.edu>
2017-06-01 18:53:59 +0200

    d

--- src/main/java/oeg/pubbyex/Servicio.java
+++ src/main/java/oeg/pubbyex/Servicio.java
@@ -10,6 +10,8 @@ import de.fuberlin.wiwiss.pubby.Configuration;
 import de.fuberlin.wiwiss.pubby.servlets.BaseServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,11 +30,27 @@ public class Servicio extends BaseServlet {
     
     public String getSPARQL(HttpServletRequest request)
     {
+            String res = request.getParameter("res");
+
+      //      String p_year = request.getParameter("p_year");
             String p_keywords = request.getParameter("p_keywords");
             String i_name = request.getParameter("i_name");
+            String i_country = request.getParameter("i_country");
+            i_country = i_country.toUpperCase();
+            if (i_country.equals("UN"))
+                i_country="";
+            
             String a_name = request.getParameter("a_name");
+            String a_country = request.getParameter("a_country");
+            a_country = a_country.toUpperCase();
+            if (a_country.equals("UN"))
+                a_country="";
+            
             String p_office = request.getParameter("p_office");
-            String p_country = request.getParameter("p_office");
+            String p_country = request.getParameter("p_country");
+            p_country = p_country.toUpperCase();
+            if (p_country.equals("UN"))
+                p_country="";
             
             String prefijos = "PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX foaf: <http://xmlns.com/foaf/0.1/> PREFIX dc: <http://purl.org/dc/elements/1.1/> PREFIX : <http://dbpedia.org/resource/> PREFIX dbpedia2: <http://dbpedia.org/property/> PREFIX dbpedia: <http://dbpedia.org/> PREFIX skos: <http://www.w3.org/2004/02/skos/core#> PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> ";
             String sparql = "_TEMPLATE1_";
@@ -42,20 +60,43 @@ public class Servicio extends BaseServlet {
             sparql += "?pat a <http://w3id.org/pmo.owl#PatentForInvention> .\n";
             sparql += "?pat <http://purl.org/dc/terms/title> ?title .\n";
             
+            //RESTRICCIONES SOBRE LA PATENTE
             if (p_office!=null &&!p_office.isEmpty()) {
                 sparql += "?pat <http://purl.org/dc/terms/publisher> \"" + p_office + "\" .";
-            }
-            if (i_name!=null && !i_name.isEmpty()) {
-                sparql += "?pat <http://w3id.org/pmo.owl#inventor> ?i .\n";
-                sparql += "?i <http://purl.org/dc/terms/title> ?iname .\n";
-            }
-            if (a_name!=null && !a_name.isEmpty()) {
-                sparql += "?pat <http://w3id.org/pmo.owl#applicant> ?a .\n";
-                sparql += "?a <http://purl.org/dc/terms/title> ?aname .\n";
             }
             if (p_country!=null && !p_country.isEmpty()) {
                 sparql += "?pat <http://w3id.org/pmo.owl#countryOfFiling> \"" + p_country + "\" .";
             }
+/*            if (p_year!=null && !p_year.isEmpty()) {
+                sparql += "?pat <http://w3id.org/pmo.owl#dateOfPublication> ?p_year .";
+            } */
+
+            //RESTRICCIONES SOBRE LA EMPRESA
+            if (!a_name.isEmpty() || !a_country.isEmpty() || res.equals("a"))
+            {
+                sparql += "?pat <http://w3id.org/pmo.owl#applicant> ?a .\n";
+            }
+            if (a_name!=null && !a_name.isEmpty()) {
+                sparql += "?a <http://purl.org/dc/terms/title> ?aname .\n";
+            }  
+            if (a_country!=null && !a_country.isEmpty()) {
+                sparql += "?a <http://www.w3.org/2006/vcard/ns#country-name> \"" + a_country + "\" .";
+            }
+             
+            //RESTRICCIONES SOBRE EL INVENTOR
+            if (!i_name.isEmpty() || !i_country.isEmpty() || res.equals("i"))
+            {
+                sparql += "?pat <http://w3id.org/pmo.owl#inventor> ?i .\n";
+                sparql += "?i <http://purl.org/dc/terms/title> ?iname .\n";
+           }
+            if (i_name!=null && !i_name.isEmpty()) {
+//                sparql += "?i <http://purl.org/dc/terms/title> ?iname .\n";
+            }
+            if (i_country!=null && !i_country.isEmpty()) {
+                sparql += "?i <http://www.w3.org/2006/vcard/ns#country-name> \"" + i_country + "\" .";
+            }
+
+            
             
             sparql+=" }"; 
             
@@ -69,7 +110,10 @@ public class Servicio extends BaseServlet {
             if (a_name!=null && !a_name.isEmpty()) {
                 sparql+="FILTER regex(?aname, \"(?:^|(?<= ))("+a_name +")(?:(?= )|$)\", \"i\") .";
             }
-            sparql+= "} LIMIT 10\n";        
+//            if (p_year!=null && !p_year.isEmpty()) {
+            
+            
+            sparql+= "} LIMIT 25\n";        
             return prefijos+sparql;
     }
     
@@ -90,15 +134,62 @@ public class Servicio extends BaseServlet {
             String answer = "";
             String sparql = getSPARQL(request);
             sparql = sparql.replace("_TEMPLATE1_", " SELECT * ");
+            
+            answer = "<!-- " + sparql +" -->\n"; 
+            
+            //p, q, a
+            String res = request.getParameter("res");
+            
+            
             Query query = QueryFactory.create(sparql);
             QueryExecution qexec = QueryExecutionFactory.sparqlService(endpoint, query);
+            Set<String> upats = new HashSet();
+            Set<String> ui = new HashSet();
+            Set<String> ua = new HashSet();
+            
             try {
                 ResultSet results = qexec.execSelect();
                 for (; results.hasNext();) {
                     QuerySolution qs = results.next();
-                    String pat = qs.get("?pat").asResource().getURI();
-                    String title = qs.get("?title").asLiteral().getLexicalForm();
-                    answer += "<a href=\""+pat+"\">"+ title + "</a><br>\n";
+                    
+                    if (res.equals("p"))
+                    {
+                        String pat = qs.get("?pat").asResource().getURI();
+                        String title = qs.get("?title").asLiteral().getLexicalForm();
+                        int size0 = upats.size();
+                        upats.add(pat);
+                        int size1 = upats.size();
+                        if (size1>size0)
+                        {
+                            answer += "<a href=\""+pat+"\">"+ title + "</a><br>\n";
+                        }
+                    }
+                    if (res.equals("i"))
+                    {
+                        String i = qs.get("?i").asResource().getURI();
+                        String iname = qs.get("?iname").asLiteral().getLexicalForm();
+                        int size0 = ui.size();
+                        ui.add(i);
+                        int size1 = ui.size();
+                        if (size1>size0)
+                        {
+                            answer += "<a href=\""+i+"\">"+ iname + "</a><br>\n";
+                        }
+                    }
+                    if (res.equals("a"))
+                    {
+                        String a = qs.get("?a").asResource().getURI();
+                        String aname = qs.get("?aname").asLiteral().getLexicalForm();
+                        int size0 = ua.size();
+                        ua.add(a);
+                        int size1 = ua.size();
+                        if (size1>size0)
+                        {
+                            answer += "<a href=\""+a+"\">"+ aname + "</a><br>\n";
+                        }
+                    }
+                    
+                    
                 }
             } catch (Exception e) {
                 PrintWriter out = response.getWriter();
